2657b61dd4e09b5ef03a4a73137ce2fa9e80b68d
Pawel Kaczmarek <pawel89@windowslive.com>
2021-08-25 07:29:07 +0200

    Aktualizacja wersji programu

--- Rozliczenie_DK_PK.py
+++ Rozliczenie_DK_PK.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 import sys
+from tkinter.constants import END
 
 window = tk.Tk()
 window.title('Obliczenia dla kredutu hipotecznego')
 window.geometry('800x600')
+window
 
 
 def close_windows():
@@ -17,32 +19,38 @@ file_menu.add_command(label='Zamknij', command=close_windows)
 window.config(menu=main_menu)
 
 
-def pk_value():
-    pk = entry_pk.get()
-    print(pk)
+def fo():
+    pk = float(entry_pk.get())
+    dk = float(entry_dk.get())
+    r = float(entry_rt.get())
+    pp = float(round(((pk * 100) / (pk + dk)) / 100, 2))
+    pd = float(round(((dk * 100) / (pk + dk)) / 100, 2))
+    rp = float(round(pp * r, 2))
+    rd = float(round(pd * r, 2))
+    wp = float(round(pk - rp, 2))
+    wd = float(round(dk - rd, 2))
+    pokaz_rp = entry_s_pk.insert(0, round(rp, 2))
+    print(pokaz_rp)
+    pokaz_rd = entry_s_dk.insert(0, round(rd, 2))
+    print(pokaz_rd)
+    pokaz_w_rp = entry_w_pk.insert(0, round(wp, 2))
+    print(pokaz_w_rp)
+    pokaz_w_rd = entry_w_dk.insert(0, round(wd, 2))
+    print(pokaz_w_rd)
+    return None
 
 
-def fo(p, d, r):
-    pp = ((p * 100) / (p + d)) / 100
-    pd = ((d * 100) / (p + d)) / 100
-    rp = int(pp * r)
-    rd = int(pd * r)
-    wp = int(p - rp)
-    wd = int(d - rd)
-    print('Wartość raty Pawła: {}'.format(rp))
-    print('Wartość raty Danieli: {}'.format(rd))
-    print('Wartość wypłaty Pawła: {}'.format(wp))
-    print('Wartość wypłaty Danieli: {}'.format(wd))
-    return "Zakończenie Obliczeń"
+def clear():
+    entry_s_pk.delete(0, END)
+    entry_s_dk.delete(0, END)
+    entry_w_pk.delete(0, END)
+    entry_w_dk.delete(0, END)
 
 
 lp = tk.Label(window, text='Wynagrodzenie Pawła: ')
 lp.pack()
 entry_pk = tk.Entry(window, width="50")
 entry_pk.pack()
-b_pk = tk.Button(window, text='Dodaj', command=pk_value)
-b_pk.pack()
-
 
 ld = tk.Label(window, text='Wynagrodzenie Danieli: ')
 ld.pack()
@@ -54,13 +62,30 @@ lr.pack()
 entry_rt = tk.Entry(window, width="50")
 entry_rt.pack()
 
-
-b_oblicz = tk.Button(window, text="Oblicz", command=pk_value and fo)
+b_oblicz = tk.Button(window, text="Oblicz", command=fo)
 b_oblicz.pack()
 
-pk = pk_value()
-dk = 2
-rk = 2
+lsp = tk.Label(window, text='Kwota kredytu Paweł: ')
+lsp.pack()
+entry_s_pk = tk.Entry(window, width="50")
+entry_s_pk.pack()
 
+lsd = tk.Label(window, text='Kwota kredytu Daniela: ')
+lsd.pack()
+entry_s_dk = tk.Entry(window, width="50")
+entry_s_dk.pack()
+
+lwp = tk.Label(window, text='Kwota do wypłaty Paweł: ')
+lwp.pack()
+entry_w_pk = tk.Entry(window, width="50")
+entry_w_pk.pack()
+
+lwd = tk.Label(window, text='Kwota do wypłaty Daniela: ')
+lwd.pack()
+entry_w_dk = tk.Entry(window, width="50")
+entry_w_dk.pack()
+
+b_wyczysc = tk.Button(window, text="Wyczyść", command=clear)
+b_wyczysc.pack()
 
 window.mainloop()
